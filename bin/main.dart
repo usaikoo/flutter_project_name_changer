44f45a35667b103b1ac958220a2c7fb386fc1ae3
usaikoo <saiko@tentaq.com>
2024-05-16 13:56:42 -0400

    fix

--- bin/main.dart
+++ bin/main.dart
@@ -1,18 +1,5 @@
-import 'package:flutter_project_name_change/flutter_project_name_change.dart';
+import 'package:flutter_project_name_changer/flutter_project_name_changer.dart';
 import 'package:args/args.dart';
-
-// void main(List<String> args) {
-//   if (args.length != 3) {
-//     print('Usage: dart rename_project.dart <old_project_name> <new_project_name> <directory>');
-//     return;
-//   }
-
-//   final oldProjectName = args[0];
-//   final newProjectName = args[1];
-//   final directory = args[2];
-
-//   renameProject(directory, oldProjectName, newProjectName);
-// }
 
 void main(List<String> arguments) {
   final parser = ArgParser();
